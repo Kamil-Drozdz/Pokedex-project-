699e6efe0f8cc00f2496d90b7ba49271b889090b
Kamil Drozdz <kamil12x@wp.pl>
2022-11-24 19:27:08 +0100

    adds try catch statement allows you to define a block of code to be tested for errors during execution

--- script.js
+++ script.js
@@ -1,64 +1,63 @@
-const pokemonName = document.querySelector(".pokemon_name");
-const pokemonNumber = document.querySelector(".pokemon_number");
-const pokemonImage = document.querySelector(".pokemon_image");
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
 
-const form = document.querySelector(".form");
-const input = document.querySelector(".input_search");
-const buttonPrev = document.querySelector(".button_prev");
-const buttonNext = document.querySelector(".button_next");
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.button_prev');
+const buttonNext = document.querySelector('.button_next');
+const displayError = document.querySelector('.err');
 
 let searchPokemon = 1;
+try {
+	const fetchPokemon = async pokemon => {
+		const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+		if (APIResponse.status === 200) {
+			const data = await APIResponse.json();
+			return data;
+		}
+	};
 
-const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-  );
+	const renderPokemon = async pokemon => {
+		pokemonName.innerHTML = 'Loading...';
 
-  if (APIResponse.status === 200) {
-    const data = await APIResponse.json();
-    return data;
-  }
-};
+		pokemonNumber.innerHTML = '';
 
-const renderPokemon = async (pokemon) => {
-  pokemonName.innerHTML = "Loading...";
-  pokemonNumber.innerHTML = "";
+		const data = await fetchPokemon(pokemon);
 
-  const data = await fetchPokemon(pokemon);
+		if (data) {
+			pokemonImage.style.display = 'block';
+			pokemonName.innerHTML = data.name;
+			pokemonNumber.innerHTML = data.id;
+			pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+			input.value = '';
+			searchPokemon = data.id;
+		} else {
+			pokemonImage.style.display = 'none';
+			pokemonName.innerHTML = 'Not found :c';
+			pokemonNumber.innerHTML = '';
+		}
+	};
 
-  if (data) {
-    pokemonImage.style.display = "block";
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src =
-      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
-        "front_default"
-      ];
-    input.value = "";
-    searchPokemon = data.id;
-  } else {
-    pokemonImage.style.display = "none";
-    pokemonName.innerHTML = "Not found :c";
-    pokemonNumber.innerHTML = "";
-  }
-};
+	form.addEventListener('submit', e => {
+		e.preventDefault();
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+		renderPokemon(input.value.toLowerCase());
+	});
 
-  renderPokemon(input.value.toLowerCase());
-});
+	buttonPrev.addEventListener('click', () => {
+		if (searchPokemon > 1) {
+			searchPokemon -= 1;
+			renderPokemon(searchPokemon);
+		}
+	});
 
-buttonPrev.addEventListener("click", () => {
-  if (searchPokemon > 1) {
-    searchPokemon -= 1;
-    renderPokemon(searchPokemon);
-  }
-});
+	buttonNext.addEventListener('click', () => {
+		searchPokemon += 1;
+		renderPokemon(searchPokemon);
+	});
 
-buttonNext.addEventListener("click", () => {
-  searchPokemon += 1;
-  renderPokemon(searchPokemon);
-});
-
-renderPokemon(searchPokemon);
+	renderPokemon(searchPokemon);
+} catch (error) {  
+	displayError.innerHTML = error;
+}
